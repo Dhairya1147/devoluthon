904c7bcad97ed81fd0d022e31b2fa7792a99abde
jangal mangal <jangal@mangal.gov>
2023-07-21 20:26:31 +0530

    first commit

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,130 @@
+  <!-- Footer Start -->
+  <footer id="rs-footer" class="rs-footer style1">
+            <div class="footer-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-12 col-sm-12 md-mb-10">
+                            <div class="footer-logo mb-40 md-mb-20">
+                                <a href="index.html"><img src="assets/images/logo-light2.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-8">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-12 col-sm-12 pl-45 md-pl-15">
+                                    <h3 class="footer-title">Address</h3>
+                                    <div class="textwidget">2096 New Market, New<br> Road
+                                    North Carolina, USA</div>
+                                </div>
+                                <div class="col-lg-4 col-md-12 col-sm-12 md-mb-10 pl-90 md-pl-15">
+                                    <h3 class="footer-title">Call Us</h3>
+                                    <div class="textwidget">
+                                        <a href="mailto:support@reactheme.com">support@reactheme.com</a><br>
+                                        <a href="tel:(+880)155-69569">(+880)155-69569</a>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-12 col-sm-12 pl-115 md-pl-15">
+                                    <h3 class="footer-title">Follow Us</h3>
+                                    <ul class="footer-social">  
+                                        <li> 
+                                            <a href="#"><i class="fa fa-facebook"></i></a>
+                                        </li>
+                                        <li> 
+                                            <a href="#"><i class="fa fa-twitter"></i></a> 
+                                        </li>
+
+                                        <li> 
+                                            <a href="#"><i class="fa fa-linkedin"></i></a>
+                                        </li>
+
+                                        <li> 
+                                            <a href="#"><i class="fa fa-instagram"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container">                    
+                    <div class="row y-middle">
+                        <div class="col-lg-6 md-mb-10 text-lg-end text-center order-last">
+                            <ul class="copy-right-menu">
+                                <li><a href="index2.html">Home2</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="schedule.html">Schedule</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="copyright text-lg-start text-center ">
+                                <p>© 2021 Evenio, All Rights Reserved. Developed By<a href="https://reactheme.com/"> reacthemes</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End -->
+
+        <!-- start scrollUp  -->
+        <div id="scrollUp" class="orange-color">
+            <i class="fa fa-angle-up"></i>
+        </div>
+        <!-- End scrollUp  -->
+
+
+        <!-- Search Modal Start -->
+        <div class="modal fade search-modal" id="searchModal" tabindex="-1">
+            <button type="button" class="close" data-bs-dismiss="modal">
+                <span class="flaticon-cross"></span>
+            </button>
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="search-block clearfix">
+                        <form>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Search Here..." type="text">
+                                <button type="submit" value="Search"><i class="flaticon-search"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Search Modal End -->
+
+        <!-- modernizr js -->
+        <script src="assets/js/modernizr-2.8.3.min.js"></script>
+        <!-- jquery latest version -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- Bootstrap v4.4.1 js -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- op nav js -->
+        <script src="assets/js/jquery.nav.js"></script>
+        <!-- isotope.pkgd.min js -->
+        <script src="assets/js/isotope.pkgd.min.js"></script>
+        <!-- owl.carousel js -->
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <!-- wow js -->
+        <script src="assets/js/wow.min.js"></script>
+        <!-- Time Circle js -->
+        <script src="assets/js/time-circle.js"></script>
+        <!-- Skill bar js -->
+        <script src="assets/js/skill.bars.jquery.js"></script>
+        <!-- imagesloaded js -->
+        <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+         <!-- waypoints.min js -->
+        <script src="assets/js/waypoints.min.js"></script>
+        <!-- counterup.min js -->
+        <script src="assets/js/jquery.counterup.min.js"></script> 
+        <!-- magnific popup js -->
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <!-- contact form js -->
+        <script src="assets/js/contact.form.js"></script>
+        <!-- main js -->
+        <script src="assets/js/main.js"></script>
+    </body>
+
+<!-- Mirrored from keenitsolutions.com/products/html/evenio/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 20 Jul 2023 09:00:34 GMT -->
+</html>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="zxx"> 
+    
+<!-- Mirrored from keenitsolutions.com/products/html/evenio/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 20 Jul 2023 09:00:33 GMT -->
+<head>
+        <!-- meta tag -->
+        <meta charset="utf-8">
+        <title>Devoluthon 2.0</title>
+        <meta name="description" content="">
+        <!-- responsive tag -->
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- favicon -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.html">
+        <link rel="shortcut icon" type="image/x-icon" href="{{url('frontend/images/fav.png')}}">
+        <!-- Bootstrap v4.4.1 css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/bootstrap.min.css')}}">
+        <!-- font-awesome css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/font-awesome.min.css')}}">
+        <!-- flaticon css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/fonts/flaticon.css')}}">
+        <!-- animate css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/animate.css')}}">
+        <!-- owl.carousel css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/owl.carousel.css')}}">
+        <!-- off canvas css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/off-canvas.css')}}">
+        <!-- magnific popup css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/magnific-popup.css')}}">
+        <!-- Main Menu css -->
+        <link rel="stylesheet" href="{{url('frontend/css/rsmenu-main.css')}}">
+        <!-- spacing css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/rs-spacing.css')}}">
+        <!-- style css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/style.css')}}"> <!-- This stylesheet dynamically changed from style.less -->
+        <!-- responsive css -->
+        <link rel="stylesheet" type="text/css" href="{{url('frontend/css/responsive.css')}}">
+        
+    </head>
+    <body class="defult-home">
+        
+        <div class="offwrap"></div>
+
+        <!--Preloader start here-->
+        
+        <!--Preloader area end here-->
+     
+		<!-- Main content Start -->
+        <div class="main-content">
+
+            <!--Full width header Start-->
+            <div class="full-width-header">
+                <!--Header Start-->
+                <header id="rs-header" class="rs-header header-transparent">
+                    <!-- Menu Start -->
+                    <div class="menu-area menu-sticky">
+                        <div class="container">
+                            <div class="row-table">
+                                <div class="col-cell header-logo">                                  
+                                    <div class="logo-area">
+                                        <a href="index.html">
+                                            <img class="normal-logo" src="{{url('frontend/images/logo-light.png')}}" alt="logo">  
+                                            <img class="sticky-logo" src="{{url('frontend/images/logo-dark.png')}}" alt="logo">
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-cell">
+                                    <div class="rs-menu-area">
+                                        <div class="main-menu">
+                                            <nav class="rs-menu hidden-md">
+                                                <ul class="nav-menu">
+                                                    <li >
+                                                        <a href="index.html">Home</a>
+                                                        
+                                                    </li>
+                                                    <li>
+                                                        <a href="#about">about</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#speaker">Speakers</a>
+                                                        
+                                                    </li>
+                                                    <li>
+                                                        <a href="#rs-events-schedule">Schedule</a>                                                        
+                                                        <ul class="sub-menu">                                                          
+                                                            
+                                                           
+                                                        </ul>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#sponsor">sponsor</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#contact">Contact</a>
+                                                    </li>
+                                                </ul> <!-- //.nav-menu -->
+                                            </nav>
+                                        </div> <!-- //.main-menu -->
+                                    </div>
+                                </div>
+                                <div class="col-cell">
+                                    <div class="expand-btn-inner">
+                                        <ul>
+                                            <li class="btn-quote">
+                                                <a href="#" class="quote-button">Buy Tickets</a>
+                                            </li>
+                                            <li class="humburger">
+                                                <a id="nav-expander" class="nav-expander bar" href="#">
+                                                    <div class="bar">
+                                                        <span class="dot1"></span>
+                                                        <span class="dot2"></span>
+                                                        <span class="dot3"></span>
+                                                        <span class="dot4"></span>
+                                                        <span class="dot5"></span>
+                                                        <span class="dot6"></span>
+                                                        <span class="dot7"></span>
+                                                        <span class="dot8"></span>
+                                                        <span class="dot9"></span>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Menu End -->
+
+                    <!-- Canvas Mobile Menu start -->
+                    <nav class="right_menu_togle mobile-navbar-menu" id="mobile-navbar-menu">
+                        <div class="close-btn">
+                            <a id="nav-close2" class="nav-close">
+                                <div class="line">
+                                    <span class="line1"></span>
+                                    <span class="line2"></span>
+                                </div>
+                            </a>
+                        </div>
+                        <ul class="nav-menu">
+                            <li class="menu-item-has-children current-menu-item">
+                                <a href="index.html">Home</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item-has-children current-menu-item">
+                                        <a href="#">Multipages</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="index.html">Home One</a></li>
+                                            <li><a href="index2.html">Home Two</a></li>
+                                            <li><a href="index3.html">Home Three</a></li>
+                                            <li><a href="index4.html">Home Four</a></li>
+                                            <li><a href="index5.html">Home Five</a></li>
+                                            <li class="active"><a href="index6.html">Home Six</a></li>
+                                        </ul> 
+                                    </li>
+                                    <li class="menu-item-has-children">
+                                        <a href="#">Onepages</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="onepage1.html">Onepage One</a></li>
+                                            <li><a href="onepage2.html">Onepage Two</a></li>
+                                            <li><a href="onepage3.html">Onepage Three</a></li>
+                                            <li><a href="onepage4.html">Onepage Four</a></li>
+                                        </ul>  
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="about.html">about</a>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Speakers</a>
+                                <ul class="sub-menu">
+                                    <li><a href="speaker1.html">Speaker One</a></li>
+                                    <li><a href="speaker2.html">Speaker Two</a></li>
+                                    <li><a href="speaker3.html">Speaker Three</a></li>
+                                    <li><a href="speaker4.html">Speaker Four</a></li>
+                                    <li><a href="speaker5.html">Speaker Five</a></li>
+                                    <li><a href="speaker-single.html">Speaker Single</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Pages</a>                                                        
+                                <ul class="sub-menu">                                                          
+                                    <li class="last-item menu-item-has-children">
+                                        <a href="#">Schedule</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="schedule-light.html">Schedule Light</a> </li>
+                                            <li><a href="Schedule-dark.html">Schedule Dark</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="gallery.html">Gallery</a></li>
+                                    <li><a href="pricing-plan.html">Pricing Plan</a></li>
+                                    <li><a href="faqs.html">Faqs</a></li>
+                                    <li><a href="Sponsors.html">Sponsors</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="blog.html">Blog</a>
+                            </li>
+                            <li>
+                                <a href="contact.html">Contact</a>
+                            </li>
+                        </ul> <!-- //.nav-menu -->
+                        <div class="canvas-contact">
+                              <div class="address-area">
+                                  <div class="address-list">
+                                      <div class="info-icon">
+                                          <i class="flaticon-location"></i>
+                                      </div>
+                                      <div class="info-content">
+                                          <h4 class="title">Address</h4>
+                                          <em>05 kandi BR. New York</em>
+                                      </div>
+                                  </div>
+                                  <div class="address-list">
+                                      <div class="info-icon">
+                                          <i class="flaticon-email"></i>
+                                      </div>
+                                      <div class="info-content">
+                                          <h4 class="title">Email</h4>
+                                          <em><a href="mailto:support@reactheme.com">support@reactheme.com</a></em>
+                                      </div>
+                                  </div>
+                                  <div class="address-list">
+                                      <div class="info-icon">
+                                          <i class="flaticon-call"></i>
+                                      </div>
+                                      <div class="info-content">
+                                          <h4 class="title">Phone</h4>
+                                          <em>+019988772</em>
+                                      </div>
+                                  </div>
+                              </div>
+                        </div>
+                    </nav>
+                    <!-- Canvas Menu end -->                             
+                </header>
+                <!--Header End-->
+            </div>
+         
